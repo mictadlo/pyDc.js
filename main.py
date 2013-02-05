59d3068736f21f5d60ca07a927b2181ac6b5fffa
mictadlo <mictadlo@gmail.com>
2013-02-05 15:17:38 +1000

    new file:   index.html 	new file:   main.py 	new file:   static/css/bootstrap-responsive.css 	new file:   static/css/bootstrap-responsive.min.css 	new file:   static/css/bootstrap.css 	new file:   static/css/bootstrap.min.css 	new file:   static/css/colorbrewer.css 	new file:   static/css/dc.css 	new file:   static/highlighter/shAutoloader.js 	new file:   static/highlighter/shBrushJScript.js 	new file:   static/highlighter/shBrushXml.js 	new file:   static/highlighter/shCore.css 	new file:   static/highlighter/shCore.js 	new file:   static/highlighter/shThemeDefault.css 	new file:   static/img/glyphicons-halflings-white.png 	new file:   static/img/glyphicons-halflings.png 	new file:   static/js/bootstrap.js 	new file:   static/js/bootstrap.min.js 	new file:   static/js/crossfilter.js 	new file:   static/js/d3.v2.js 	new file:   static/js/dc.js 	new file:   static/ndx.csv

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[(r'/', IndexHandler)],
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True)
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
