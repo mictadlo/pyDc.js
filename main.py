5aed9261f7afa7c1d55b4984b97f8d29a3534053
mictadlo <mictadlo@gmail.com>
2013-02-22 15:29:36 +1000

    modified:   README.md 	new file:   crime/canada_map.svg 	new file:   crime/crime.csv 	new file:   crime/filter_stats.rb 	new file:   crime/index.html 	modified:   index.html 	modified:   main.py 	new file:   static/img/us-states.json 	new file:   vc/index.html 	new file:   vc/state2code.rb 	new file:   vc/vc.csv

--- main.py
+++ main.py
@@ -21,7 +21,55 @@ class NDXHandler(tornado.web.RequestHandler):
     def get(self):
         db = self.settings['db']
 
-        cursor = db.things.find({}, fields={'_id': False}).sort('_id')
+        cursor = db.ndx.find({}, fields={'_id': False}).sort('_id')
+
+        data = yield motor.Op(cursor.to_list)
+
+        response_str = json.dumps(data, default=json_util.default)
+
+        self.set_header('Content-Type', 'application/json')
+        self.write(response_str)
+        self.finish()
+
+
+class CrimeHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        self.render('crime/index.html')
+
+
+class CrimeDbHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        db = self.settings['db']
+
+        cursor = db.crime.find({}, fields={'_id': False}).sort('_id')
+
+        data = yield motor.Op(cursor.to_list)
+
+        response_str = json.dumps(data, default=json_util.default)
+
+        self.set_header('Content-Type', 'application/json')
+        self.write(response_str)
+        self.finish()
+
+
+class VcHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        self.render('vc/index.html')
+
+
+class VcDbHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        db = self.settings['db']
+
+        cursor = db.vc.find({}, fields={'_id': False}).sort('_id')
 
         data = yield motor.Op(cursor.to_list)
 
@@ -36,7 +84,13 @@ if __name__ == '__main__':
     db = motor.MotorClient().open_sync().test
 
     app = tornado.web.Application(
-        handlers=[(r'/', IndexHandler), (r'/ndx', NDXHandler)],
+        handlers=[(r'/', IndexHandler),
+                  (r'/ndx', NDXHandler),
+                  (r'/crime', CrimeHandler),
+                  (r'/crimedb', CrimeDbHandler),
+                  (r'/vc', VcHandler),
+                  (r'/vcdb', VcDbHandler)
+                  ],
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=True, db=db)
 
